2c9c32ae534fc78de61920c9f7bf0e7483b27da7
Rishab <110557273+Shabb03@users.noreply.github.com>
2024-02-24 16:41:30 +0000

    Delete sampleList.py
    Unneccessary

--- sampleList.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from random import sample
-
-max = input("Enter the maximum number: ")
-count = input("Enter the size of the list")
-
-rnums = sample(range(max), count)
-print(rnums)
