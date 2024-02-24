ccb705424da48fa3043fecb8dfd4d510d1226c0c
Rishab <110557273+Shabb03@users.noreply.github.com>
2024-02-24 16:44:12 +0000

    Delete factorial.py
    Unneccessary

--- factorial.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def factorial(n):
-    if n == 1 or n == 0:
-        return 1
-    return n * factorial(n-1)
-
-number = int(input("Enter a number: "))
-print(factorial(number))
