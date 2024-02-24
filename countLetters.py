5a0ad26524af5f6fef456db906360b7bfae58c12
Rishab <110557273+Shabb03@users.noreply.github.com>
2024-02-24 16:43:20 +0000

    Delete countLetters.py
    Unneccessary

--- countLetters.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def countLetters(s):
-    if s == "":
-        return 0
-    return 1 + countLetters(s[:-1])
-
-string = input("Enter a word: ")
-print(countLetters(string))
